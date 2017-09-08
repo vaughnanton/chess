1a93701c724a560ccbd795a0c8d58bcd1f789875
Vaughn Antonyan <vaughnanton@gmail.com>
2017-09-07 19:47:28 -0700

    Add unicode for playing pieces

--- lib/pieces.rb
+++ lib/pieces.rb
@@ -8,21 +8,68 @@ class Pieces
   end
 
   def king
+    if @color == 'black'
+			#Unicode for black rook
+			@piece = "\u{265A}"
+
+		elsif @color == 'white'
+			#Unicode for white rook
+			@piece = "\u{2654}"
+		end
   end
 
   def queen
+    if @color == 'black'
+      #Unicode for black rook
+      @piece = "\u{265B}"
+
+    elsif @color == 'white'
+      #Unicode for white rook
+      @piece = "\u{2655}"
+    end		
   end
 
   def rook
+    if @color == 'black'
+			#Unicode for black rook
+			@piece = "\u{265C}"
+
+		elsif @color == 'white'
+			#Unicode for white rook
+			@piece = "\u{2656}"
+		end
   end
 
   def bishop
+    if @color == 'black'
+			#Unicode for black rook
+			@piece = "\u{265D}"
+
+		elsif @color == 'white'
+			#Unicode for white rook
+			@piece = "\u{2657}"
+		end
   end
 
   def knight
+    if @color == 'black'
+      #Unicode for black rook
+    @piece = "\u{265E}"
+
+    elsif @color == 'white'
+      #Unicode for white rook
+      @piece = "\u{2658}"
+    end
   end
 
   def pawn
+    if @color == 'black'
+			#Unicode for black rook
+			@piece = "\u{265F}"
+		elsif @color == 'white'
+			#Unicode for white rook
+			@piece = "\u{2659}"
+		end
   end
 
 end
